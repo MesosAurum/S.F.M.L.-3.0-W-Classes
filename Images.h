ddf666be56c2e4398067cc7008bccf69f6ebfc14
MesosAurum <mesosaurum@yahoo.com>
2025-04-04 10:23:48 -0700

    Create Images.h

---
+++ Images.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Images : public sf::Image {
+
+private:
+
+	sf::Texture *texture;
+	sf::Sprite *sprite;
+
+	void initTexture();
+	void initSprite();
+
+public:
+
+	Images(sf::Vector2u image_size);
+	~Images();
+
+	void updateTexture();
+	sf::Sprite getSprite();
+};
