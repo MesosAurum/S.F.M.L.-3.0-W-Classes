232cb9391c2725c829c10133fd7106f293d34970
MesosAurum <mesosaurum@yahoo.com>
2025-04-04 10:22:42 -0700

    Create Fonts.cpp

---
+++ Fonts.cpp
@@ -0,0 +1,18 @@
+#include "Fonts.h"
+
+void Fonts::initFonts(std::string address) {
+
+	if(!this->openFromFile(address)) {
+
+		std::cerr << "ERROR::FONTS::INITFONTS::COULD NOT LOAD FILE" << address << std::endl;
+	}
+	this->setSmooth(false);
+}
+Fonts::Fonts(std::string address) {
+
+	initFonts(address);
+}
+Fonts::~Fonts() {
+
+
+}
