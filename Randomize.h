ae573f32415f7801e64fa137f0cc27f6cdb34ace
MesosAurum <mesosaurum@yahoo.com>
2025-04-04 10:24:37 -0700

    Create Randomize.h

---
+++ Randomize.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <random>
+#include "times.h"
+
+class Randomize {
+
+private:
+
+	Times times;
+
+	std::default_random_engine randomEngine;
+
+	std::uniform_int_distribution<int> randomInt;
+	std::uniform_real_distribution<float> randomFloat;
+
+	void initRandomEngine();
+
+public:
+
+	Randomize();
+	~Randomize();
+
+	void randomizeInt(int min, int max);
+	void randomizeFloat(float min, float max);
+	int randomI();
+	float randomF();
+};
