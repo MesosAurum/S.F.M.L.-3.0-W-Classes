4d24c6a2c3d0a2630a17b1b6f4b0de6e9ca393ce
MesosAurum <mesosaurum@yahoo.com>
2025-05-14 15:42:34 -0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,36 +2,20 @@
 
 void Game::initWindow() {
 
-	width = 640;
-	height = 360;
+	windowSize.x = 640.0f;
+	windowSize.y = 360.0f;
 	title = "SFML with classes";
-	vm = sf::VideoMode({ width, height });
+	vm = sf::VideoMode(sf::Vector2u(windowSize));
 	window = new sf::RenderWindow(vm, title);
 	window->setFramerateLimit(60);
 }
-void Game::initTexts() {
-
-	texts = new Texts("dt: 0.000000", "Fonts/arial.ttf");
-	texts->setOrigin(texts->getGlobalBounds().size / 2.0f);
-	texts->setPosition({ width / 2.0f, height / 2.0f });
-}
-void Game::initImages() {
-
-	images = new Images({ width,height });
-}
-void Game::initRandomize() {
-
-	randomize = new Randomize();
-}
 void Game::initGOL() {
 
-	gol = new GOL({ width,height });
+	gol = new GOL(sf::Vector2u(windowSize));
 }
-void Game::initSprites() {
+void Game::initSpaceRocks() {
 
-	sprites = new Sprites("Sprites/AnimationExample.png", { 32,32 });
-	sprites->setPosition({ width / 2.0f, height / 2.0f });
-	sprites->setFrameTimer(0.1f);
+	spaceRocks = new SpaceRocks(windowSize);
 }
 void Game::pollEvents() {
 
@@ -54,42 +38,16 @@ void Game::updating() {
 
 	pollEvents();
 
-	times.nextDeltaTime();
-
-	float dt = times.getDeltaTime();
-
 	//gol->update();
-
-	sprites->animate(dt);
-
-	//randomize->randomizeFloat(0.0f, 10.0f);
-	//float randVar = randomize->randomF();
-
-	//texts->streamText("Random number: ", randVar);
-
-	//for(unsigned int y = 0; y < height; ++y) {
-	//	for(unsigned int x = 0; x < width; ++x) {
-	//
-	//		std::uint8_t c = (x & y) ^ x;
-	//		c *= 255;
-	//		sf::Color color = sf::Color(c, c, c);
-	//
-	//		images->setPixel({ x,y }, color);
-	//	}
-	//}
-	//images->updateTexture();
+	spaceRocks->update(windowSize);
 }
 void Game::rendering() {
 
 	window->clear();
 
 	//Draw here!!!
-	//window->draw(*texts);
-	//window->draw(images->getSprite());
-
 	//window->draw(gol->getSprite());
-
-	window->draw(*sprites);
+	spaceRocks->render(*window);
 
 	window->display();
 }
@@ -97,19 +55,13 @@ Game::Game() {
 
 	initWindow();
 	//initGOL();
-	//initTexts();
-	//initImages();
-	//initRandomize();
-	initSprites();
+	initSpaceRocks();
 }
 Game::~Game() {
 
-	delete texts;
-	delete images;
-	delete randomize;
-	delete gol;
-	delete sprites;
+	//delete gol;
 	delete window;
+	delete spaceRocks;
 }
 void Game::running() {
 
