59402952b4e9d917a373f798b2cc0f3b85ecbe0c
MesosAurum <mesosaurum@yahoo.com>
2025-06-21 23:22:26 -0700

    Update SpaceRocks.cpp

--- SpaceRocksFolder/SpaceRocks.cpp
+++ SpaceRocksFolder/SpaceRocks.cpp
@@ -11,26 +11,40 @@ void SpaceRocks::initShip(sf::Vector2f window_size) {
 
 	ship.setPosition(window_size / 2.0f);
 }
+void SpaceRocks::initHealth() {
+
+	health = 100;
+}
+void SpaceRocks::initHealthbar(sf::Vector2f window_size) {
+
+	healthbar = new HealthBar(health, window_size);
+}
 SpaceRocks::SpaceRocks(sf::Vector2f window_size) {
 
 	dt = 0.0f;
 	initScoreText(window_size);
+	initHealth();
+	initHealthbar(window_size);
 	initShip(window_size);
 }
 SpaceRocks::~SpaceRocks() {
 
 	delete scoreText;
+	delete healthbar;
 }
 void SpaceRocks::update(sf::Vector2f window_size) {
 
 	times.nextDeltaTime();
 	dt = times.getDeltaTime();
 
-	ship.update(dt, window_size);
-	pellets.firePellet(ship.getRotation(), ship.getPosition());
-	pellets.update(dt, window_size);
-	rocks.update(dt, window_size);
+	if(health > 0) {
 
+		ship.update(dt, window_size);
+		pellets.firePellet(ship.getRotation(), ship.getPosition());
+		pellets.update(dt, window_size);
+		rocks.update(dt, window_size, ship, pellets, score, health);
+		healthbar->updateHealth(health);
+	}
 	scoreText->streamText("Score: ", score);
 }
 void SpaceRocks::render(sf::RenderWindow &window) {
@@ -39,4 +53,5 @@ void SpaceRocks::render(sf::RenderWindow &window) {
 	pellets.render(window);
 	ship.render(window);
 	rocks.render(window);
+	healthbar->render(window);
 }
