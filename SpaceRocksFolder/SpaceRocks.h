5d1d43980ceb2aff4dcaabbc7bb4a083e6a17226
MesosAurum <mesosaurum@yahoo.com>
2025-07-01 19:55:29 -0700

    Update SpaceRocks.h

--- SpaceRocksFolder/SpaceRocks.h
+++ SpaceRocksFolder/SpaceRocks.h
@@ -5,6 +5,7 @@
 #include "Pellets.h"
 #include "Rocks.h"
 #include "HealthBar.h"
+#include "Stars.h"
 
 class SpaceRocks {
 
@@ -12,18 +13,22 @@ private:
 
 	Times times;
 	Texts *scoreText;
+	Texts *endText;
 
 	Ship ship;
 	Pellets pellets;
 	Rocks rocks;
 	HealthBar *healthbar;
+	Stars *stars;
 
 	float dt;
 	std::uint16_t score;
 	int health;
 
 	void initScoreText(sf::Vector2f window_size);
+	void initEndText(sf::Vector2f window_size);
 
+	void initStars(sf::Vector2f window_size);
 	void initShip(sf::Vector2f window_size);
 	void initHealth();
 	void initHealthbar(sf::Vector2f window_size);
