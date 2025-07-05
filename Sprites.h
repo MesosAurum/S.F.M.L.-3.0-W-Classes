93fff98f5b346ba196749aa34fdfaec0fc0da5d1
MesosAurum <mesosaurum@yahoo.com>
2025-07-05 12:39:38 -0700

    Update Sprites.h

--- Sprites.h
+++ Sprites.h
@@ -4,25 +4,15 @@
 class Sprites : public sf::Sprite {
 
 private:
-
-	Textures *textures;
+	
 	sf::Texture tempTex;
-
-	sf::Vector2i texCoord;
-
-	float frameTimer;
-	float frameTimerMax;
-
-	void initTimers();
 
 public:
 
+	Textures *textures;
+
 	Sprites(std::string sprite_address = "", sf::Vector2u sprite_size = { 16, 16 });
 	~Sprites();
-
-	void frameRight();
-	void frameLeft();
+	
 	void animate(float dt = 0.016667);
-
-	void setFrameTimer(float frame_time);
 };
