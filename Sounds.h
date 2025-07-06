fba367fe33ee76c40a1fb9176632b4ab0c22f13b
MesosAurum <mesosaurum@yahoo.com>
2025-07-05 22:20:00 -0700

    Update Sounds.h

--- Sounds.h
+++ Sounds.h
@@ -60,6 +60,10 @@ private:
 	Randomize *randomize;
 	sf::SoundBuffer *soundBuffer;
 
+	//Frequencies are generated with the function 2^((x-57)/12).
+	//x is note.
+	//y is octave.
+	//Starts with C_0 to B_8
 	float frequencyTable[12][9];
 	float amplitude = 0.2;
 	float beatsPerMinute;
@@ -70,12 +74,24 @@ private:
 	void initFrequencyTable();
 	void initRandomize();
 	sf::SoundBuffer *initBuffer(std::string address);
+	//Types: 0 = sine wave, 1 = square wave, 3 = triangle wave, 4 = sawtooth wave, otherwise white noise.
+	//Amplitude: [0.0 ... 1.0].
 	void generateSound();
 
 public:
 
-
+	//Input string address to insert custom sound.
 	Sounds(std::string buffer_address = "");
+	//Note is {0 to 11 : Pitch, 0 to 8 : Octaves, 0 to 7 : Value}.
+	//Pitch is base frequency before multiplying with octaves.
+	//Octaves may be thought as a multiplier for pitch.
+	//Value is how long a tone is played: 0 = 8*, 1 = 4*, 2 = 2*, 3 = 1*, 4 = 0.5*, etc.
+	//x being the note.
+	//y being the octave.
+	//z is note value.
+	//Waveform is 0 - 4: 0 = Sine, 1 = Square, 2 = Triangle,3 = SawWave, 4 = noise.
+	//Amplitude is 0.0 - 1.0, this is volume.
+	//BPM is beats per minute 0 - 255.
 	Sounds(std::vector<sf::Vector3i> note, unsigned int waveform = 0, float amplitude = 0.0f, unsigned short bpm = 60);
 	~Sounds();
 
