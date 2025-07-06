7a655b90f90e39f375b5e673320cd1c98b9fdfd9
MesosAurum <mesosaurum@yahoo.com>
2025-07-05 22:13:53 -0700

    Update Times.h

--- Times.h
+++ Times.h
@@ -13,7 +13,11 @@ public:
 	Times();
 	~Times();
 
+	//To be placed at the start of update loops
+	//Will set dt (delta time) as the time elapsed since last function call
+	//dt is in seconds
 	void nextDeltaTime();
 	float getDeltaTime() const;
+	//Returns time since epoch
 	int getDtEpoch();
 };
