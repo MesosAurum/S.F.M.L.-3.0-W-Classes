95dfc116d9372878e064887b94d774c0f7a5d705
MesosAurum <mesosaurum@yahoo.com>
2025-07-14 10:27:26 -0700

    Update Textures.cpp

--- Textures.cpp
+++ Textures.cpp
@@ -25,19 +25,13 @@ Textures::~Textures() {
 }
 void Textures::frameRight() {
 
-	this->texCoord.x += this->getFrameSize().x;
-	if(this->texCoord.x >= this->getSize().x) {
-
-		this->texCoord.x = 0;
-	}
+	float nextX = texCoord.x + this->getFrameSize().x;
+	this->texCoord.x = fmod(nextX, this->getSize().x);
 }
 void Textures::frameLeft() {
 
-	this->texCoord.x -= this->getFrameSize().x;
-	if(this->texCoord.x < 0) {
-
-		this->texCoord.x = this->getSize().x - this->getFrameSize().x;
-	}
+	float nextX = texCoord.x - this->getFrameSize().x;
+	this->texCoord.x = fmod(nextX, frameSize.x + this->getSize().x);
 }
 float Textures::getFrameTimer() const {
 
@@ -55,9 +49,21 @@ void Textures::setFrameTimerMax(float frame_time) {
 
 	frameTimerMax = frame_time;
 }
+void Textures::changeAnimation(unsigned int animation_number) {
+
+	sf::Vector2i coordinate;
+	coordinate.x = 0.0f;
+	coordinate.y = animation_number * this->getFrameSize().y;
+
+	texCoord = coordinate;
+}
 sf::Vector2i Textures::getTexCoord() const {
 
 	return texCoord;
+}
+void Textures::setTexCoord(sf::Vector2i coord) {
+
+	texCoord = coord;
 }
 sf::Vector2u Textures::getFrameSize() const {
 
