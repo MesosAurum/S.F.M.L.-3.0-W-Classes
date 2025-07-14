25118e457e4cf03f0b713fa882efcbe44665ee51
MesosAurum <mesosaurum@yahoo.com>
2025-07-14 10:26:28 -0700

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,52 +1,19 @@
 #include "Entity.h"
 
-void Entity::tranlateEntity() {
+Entity::Entity(std::string address, sf::Vector2u size) : texSize(size) {
 
-	float x = getPosition().x;
-	float y = getPosition().y;
-	float w = entitySize.x;
-	float h = entitySize.y;
-
-	vertArray[0].position = { x - origin.x, y - origin.y };
-	vertArray[1].position = { x - origin.x , y + h - origin.y };
-	vertArray[2].position = { x + w - origin.x, y - origin.y };
-	vertArray[3].position = { x + w - origin.x, y + h - origin.y };
-}
-void Entity::rotate() {
-
-	float theta = getRotation();
-	float c = cos(theta);
-	float s = sin(theta);
-	float cx = getPosition().x;
-	float cy = getPosition().y;
-	for(int i = 0; i < 4; ++i) {
-
-		float px = vertArray[i].position.x;
-		float py = vertArray[i].position.y;
-
-		px -= cx;
-		py -= cy;
-
-		float xNew = px * c - py * s + cx;
-		float yNew = px * s + py * c + cy;
-
-		vertArray[i].position = { xNew , yNew };
-	}
-}
-Entity::Entity(std::string address, sf::Vector2u size) : entitySize(size) {
-
-	texSize = entitySize;
-
-	setRadii(sf::Vector2f(size));
+	this->setRadii(sf::Vector2f(size));
 
 	vertArray.setPrimitiveType(sf::PrimitiveType::TriangleStrip);
 	vertArray.resize(4);
 
-	tranlateEntity();
-
-	setMomentOfIntertia(sf::Vector2f(size));
+	this->setMomentOfInertia(sf::Vector2f(size));
 
 	shiftAnimation({ 0.0f, 0.0f });
+
+	this->setOrigin(texSize / 2.0f);
+
+	resizeArray();
 
 	textures = new Textures(address, size);
 }
@@ -56,15 +23,15 @@ Entity::~Entity() {
 }
 void Entity::animate(float dt) {
 
-	textures->setFrameTimer(textures->getFrameTimer() - dt);
-	if(textures->getFrameTimer() <= 0.0f) {
+	this->textures->setFrameTimer(this->textures->getFrameTimer() - dt);
+	if(this->textures->getFrameTimer() <= 0.0f) {
 
-		textures->frameRight();
+		this->textures->frameRight();
 		sf::Vector2i texCoord = this->textures->getTexCoord();
 
 		shiftAnimation(sf::Vector2f(texCoord));
 
-		textures->setFrameTimer(textures->getFrameTimerMax());
+		this->textures->setFrameTimer(this->textures->getFrameTimerMax());
 	}
 }
 void Entity::shiftAnimation(sf::Vector2f shift) {
@@ -72,10 +39,10 @@ void Entity::shiftAnimation(sf::Vector2f shift) {
 	float cx = shift.x + texSize.x;
 	float cy = shift.y + texSize.y;
 
-	vertArray[0].texCoords = { shift.x , shift.y };
-	vertArray[1].texCoords = { shift.x,cy };
-	vertArray[2].texCoords = { cx , shift.y };
-	vertArray[3].texCoords = { cx,cy };
+	vertArray[0].texCoords = { shift.x, shift.y };
+	vertArray[1].texCoords = { shift.x, cy };
+	vertArray[2].texCoords = { cx, shift.y };
+	vertArray[3].texCoords = { cx, cy };
 }
 sf::Color Entity::getColor() {
 
@@ -90,23 +57,28 @@ void Entity::setColor(sf::Color color) {
 }
 void Entity::setFrameTimerMax(float time) {
 
-	textures->setFrameTimerMax(time);
+	this->textures->setFrameTimerMax(time);
 }
-sf::Vector2f Entity::getEntitySize() const {
+sf::Vector2f Entity::getOrigin() const {
 
-	return entitySize;
-}
-void Entity::setEntitySize(sf::Vector2f size) {
-
-	entitySize = size;
-}
-sf::Vector2f Entity::getOrigin() {
-
-	return origin;
+	return this->origin;
 }
 void Entity::setOrigin(sf::Vector2f origin) {
 
 	this->origin = origin;
+	resizeArray();
+}
+void Entity::resizeArray() {
+
+	float x = -this->getOrigin().x;
+	float y = -this->getOrigin().y;
+	float w = texSize.x;
+	float h = texSize.y;
+
+	vertArray[0].position = { x, y };
+	vertArray[1].position = { x , y + h };
+	vertArray[2].position = { x + w, y };
+	vertArray[3].position = { x + w, y + h };
 }
 sf::FloatRect Entity::getLocalBounds() {
 
@@ -115,17 +87,20 @@ sf::FloatRect Entity::getLocalBounds() {
 sf::FloatRect Entity::getGlobalBounds() {
 
 	sf::FloatRect globalBounds = getLocalBounds();
-	globalBounds.position = this->getPosition();
+	globalBounds.position = this->getPosition() + this->getOrigin();
 	return globalBounds;
+}
+sf::Vector2f Entity::getTextureSize() const {
+
+	return texSize;
 }
 void Entity::updateEntity(float dt, sf::Vector2f window_size) {
 
-	tranlateEntity();
-	rotate();
 	update(dt, window_size);
 }
 void Entity::draw(sf::RenderTarget &target, sf::RenderStates states) const{
 
+	states.transform *= getTransform();
 	states.texture = &*textures;
 	target.draw(vertArray, states);
 }
