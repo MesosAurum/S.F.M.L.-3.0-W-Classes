438c9e7d6c53f1d66639f66c38a46e6d6c307401
MesosAurum <mesosaurum@yahoo.com>
2025-07-18 19:48:54 -0700

    Update Rocks.cpp

--- SpaceRocksFolder/Rocks.cpp
+++ SpaceRocksFolder/Rocks.cpp
@@ -16,15 +16,13 @@ void Rocks::initColors() {
 
 	colors = new Colors();
 }
-void Rocks::spawnRock(sf::Vector2f pos, sf::Vector2f rock_size, sf::Vector2f window_size) {
+void Rocks::spawnRock(sf::Vector2f pos, sf::Vector2f window_size) {
 
 	rocks.push_back(new Entity("Sprites/meteoroid.png", textureSize));
 
 	unsigned int index = rocks.size() - 1;
 
-	rocks.at(index)->setEntitySize(rock_size);
-	rocks.at(index)->setOrigin(rocks.at(index)->getEntitySize() / 2.0f);
-
+	rocks.at(index)->setOrigin(sf::Vector2f(textureSize) / 2.0f);
 	rocks.at(index)->setPosition(pos);
 
 	randomize->randomizeFloat(0.0f, 360.0f);
@@ -84,7 +82,7 @@ void Rocks::update(float dt, sf::Vector2f window_size, Entity &ship, Pellets &pe
 			pos.x = -2.0f * float(textureSize.x);
 			pos.y = randomize->randomF();
 		}
-		spawnRock(pos, sf::Vector2f(textureSize), window_size);
+		spawnRock(pos, window_size);
 		rockTimer = rockTimerMax;
 	}
 	for(int i = rocks.size() - 1; i >= 0; --i) {
@@ -93,28 +91,32 @@ void Rocks::update(float dt, sf::Vector2f window_size, Entity &ship, Pellets &pe
 
 		if(ship.getGlobalBounds().findIntersection(rocks.at(i)->getGlobalBounds())) {
 
-			health -= rocks.at(i)->getEntitySize().x;
+			health -= int(rocks.at(i)->getScale().x * textureSize.x);
 			deleteRock(i);
 			break;
 		}
 		for(int j = pellets.getPellets().size() - 1; j >= 0; --j) {
-		
+
 			if(pellets.getPellets().at(j)->getGlobalBounds().findIntersection(rocks.at(i)->getGlobalBounds())) {
-		
+
 				score += 16;
-		
+
 				delete pellets.getPellets().at(j);
 				pellets.getPellets().erase(pellets.getPellets().begin() + j);
-		
-				if(rocks.at(i)->getEntitySize().x < 10.0f) {
-		
+
+				if(rocks.at(i)->getScale().x * textureSize.x < 10.0f) {
+
 					deleteRock(i);
 					break;
 				}
-				rocks.at(i)->setEntitySize(rocks.at(i)->getEntitySize() / 2.0f);
-				spawnRock(rocks.at(i)->getPosition(), rocks.at(i)->getEntitySize(), window_size);
-				rocks.at(rocks.size() - 1)->setColor(rocks.at(i)->getColor());
-				rocks.at(rocks.size() - 1)->updateEntity(dt, window_size);
+				rocks.at(i)->scale({ 0.5f, 0.5f });
+				spawnRock(rocks.at(i)->getPosition(), window_size);
+
+				unsigned int index = rocks.size() - 1;
+
+				rocks.at(index)->setColor(rocks.at(i)->getColor());
+				rocks.at(index)->updateEntity(dt, window_size);
+				rocks.at(index)->setScale(rocks.at(i)->getScale());
 				break;
 			}
 		}
